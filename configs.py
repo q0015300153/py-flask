a41cbf409c1c1a96fac8bdbc99677ad429053b44
TB <q0015300153@gmail.com>
2022-10-18 16:01:20 +0800

    更新

--- configs.py
+++ configs.py
@@ -3,16 +3,17 @@ import os
 import pymysql.cursors
 from dotenv import load_dotenv
 
+load_dotenv()
+DB_HOST = os.getenv('DB_HOST', 'localhost')
+DB_USER = os.getenv('DB_HOST', 'localhost')
+DB_PASSWORD = os.getenv('DB_HOST', 'localhost')
+DB_STORE = os.getenv('DB_HOST', 'localhost')
+
 
 class Db:
     def __init__(self):
-        load_dotenv()
-        self.db = pymysql.Connection(host = os.getenv("DB_HOST"),
-                                     user = os.getenv("DB_USER"),
-                                     password = os.getenv("DB_PASSWORD"),
-                                     db = os.getenv("DB_STORE"),
-                                     charset = 'utf8mb4',
-                                     cursorclass = pymysql.cursors.DictCursor)
+        self.db = pymysql.Connection(host = DB_HOST, user = DB_USER, password = DB_PASSWORD, db = DB_STORE,
+                                     charset = 'utf8mb4', cursorclass = pymysql.cursors.DictCursor)
 
     def __del__(self):
         self.db.close()
